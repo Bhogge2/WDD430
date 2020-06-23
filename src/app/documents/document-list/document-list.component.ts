3603fd311c8c890131282a788b4055dbade6ccc9
Bhogge2 <35310873+Bhogge2@users.noreply.github.com>
2020-06-23 11:03:49 -0700

    added document view

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  document: Document[] = [
+    new Document('1', 'Document 1', 'Document Description 1','documentURL@byui.edu', null),
+    new Document('2', 'Document 2', 'Document Description 2','documentURL2@byui.edu', null)
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onSelected(document: Document) {
+    this.selectedDocumentEvent.emit(document);
   }
 
 }
